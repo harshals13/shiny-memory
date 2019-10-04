7666351447546c368200360c4a772f42cdb3e5a5
Harsha <harshasuryavamshi@gmail.com>
2019-10-04 22:32:49 +0530

    added db connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://harshals13:T9IL817ngqqtQY6f@cluster0-4mxkf.mongodb.net/admin?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB connection Succeeded') }
     else { console.log('Error in DB conncetion : ' + err)}
 });
